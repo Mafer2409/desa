c0ed3e5a12153944f85a6afdf23f6b1c26b866b9
Mafer2409 <manfdz70@gmail.com>
2023-06-22 23:30:49 +0800

    new

--- rt/main.php
+++ rt/main.php
@@ -59,7 +59,7 @@ if (!isset($_SESSION['id_user'])) {
 
     <!-- Navbar Start -->
     <nav class="navbar navbar-expand-lg navbar-dark fixed-top py-lg-0 px-lg-5 wow fadeIn" data-wow-delay="0.1s">
-        <a href="index.html" class="navbar-brand ms-4 ms-lg-0">
+        <a href="?page=home" class="navbar-brand ms-4 ms-lg-0">
             <h1 class="text-primary m-0">DESA</h1>
         </a>
         <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
